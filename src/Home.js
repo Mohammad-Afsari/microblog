3e4f7bdf409057b22a0b031ee9d19b552ded8cba
Mo <mo2005@hotmail.co.uk>
2022-02-16 18:17:55 +0000

    Router initialised

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,15 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => res.json())
-            .then(data => setBlogs(data))
-    },[])
-
-
-    // useEffect hook:
-    // This hook fires a callback function everytime the component re-renders and state changes 
-
-    // [] -> Second argument in the useEffect Hook adds a dependancy which in this specific case, none are passed through hence the callback won't run after it's initial page load. By default just leaving [] in the dependancy will mean it will not watch over anything and that the state only will run on page load.
-
-    // We can pass dependancies into the second argument array to watch over certain states e.g. name
-
-    return ( // props are named as pop.name={javascript function}
+    return (
         <div className="home">
+            {/* Conditional template */}
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
      );
